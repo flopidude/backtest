cd99a7cbe75b30c457f656eeaba972d6aeef4579
flopius <flopodude@gmail.com>
2024-03-15 22:50:41 +0200

    V1.2
    Proper rendering of multiple asynchronous downloads
    Making the download process more logical - if the fallback date is earlier than the earliest, download since that date to prevent unwanted behaviour.
    Several bug fixes.

--- src/historical_binance.py
+++ src/historical_binance.py
@@ -55,11 +55,13 @@ class BinanceDataProvider:
     async def update_tickers(self, pairs: [str], timeframes: [str], fallback_starting_date: datetime = None):
         """
         This function downloads the new data for the given pairs and timeframes and merges it with the existing data.
-        If there is completely no data available, it downloads the data since the fallback_starting_date.
+        If there is completely no data available or the earliest available date is later than the fallback starting date, it downloads the data since the fallback_starting_date.
         """
         to_datetime = datetime.combine(date.today(), datetime.min.time())
         if fallback_starting_date is None:
-            fallback_starting_date = datetime.combine(date.today() - relativedelta(years=2), datetime.min.time())
+            fallback_starting_date = datetime.combine(date.today() - relativedelta(years=2), datetime.min.time()).replace(tzinfo=UTC)
+        else:
+            fallback_starting_date = fallback_starting_date.replace(tzinfo=UTC)
         for pair in pairs:
             ticker = pair.replace("/USDT:USDT", "USDT")
             for timeframe in timeframes:
@@ -68,12 +70,14 @@ class BinanceDataProvider:
                 # Get the last available date
                 if self.cached_dataframes[timeframe][pair] is not None and not self.cached_dataframes[timeframe][
                     pair].is_empty():
-                    _, last_date = self.fetch_dataframe_constraints(pair, timeframe)
+                    earliest_date, download_from = self.fetch_dataframe_constraints(pair, timeframe)
+                    if earliest_date.replace(tzinfo=UTC) > fallback_starting_date:
+                        download_from = fallback_starting_date
                 else:
-                    last_date = fallback_starting_date
+                    download_from = fallback_starting_date
 
                 # Download new data and merge with existing data
-                new_data = await self.data_downloader.download_one_ticker(ticker, last_date, to_datetime, timeframe)
+                new_data = await self.data_downloader.download_one_ticker(ticker, download_from, to_datetime, timeframe)
                 if new_data is not None and not new_data.is_empty():
                     if self.cached_dataframes[timeframe][pair] is not None and not self.cached_dataframes[timeframe][
                         pair].is_empty():
@@ -83,7 +87,7 @@ class BinanceDataProvider:
                     else:
                         self.cached_dataframes[timeframe][pair] = new_data
                     self.cached_dataframes[timeframe][pair].write_csv(ticker_path)
-                    print(f"Updated data for {pair}")
+                    # print(f"Updated data for {pair}")
                 else:
                     print(f"No new data available for {pair}")
 
@@ -95,7 +99,9 @@ class BinanceDataProvider:
         """
         today_datetime = datetime.combine(date.today(), datetime.min.time())
         if fallback_starting_date is None:
-            fallback_starting_date = datetime.combine(date.today() - relativedelta(years=2), datetime.min.time())
+            fallback_starting_date = datetime.combine(date.today() - relativedelta(years=2), datetime.min.time()).replace(tzinfo=UTC)
+        else:
+            fallback_starting_date = fallback_starting_date.replace(tzinfo=UTC)
         tasks = []
         properties = []
         for timeframe in timeframes:
@@ -105,13 +111,15 @@ class BinanceDataProvider:
                 # Get the last available date
                 if self.cached_dataframes[timeframe][pair] is not None and not self.cached_dataframes[timeframe][
                     pair].is_empty():
-                    _, last_date = self.fetch_dataframe_constraints(pair, timeframe)
+                    earliest_date, download_from = self.fetch_dataframe_constraints(pair, timeframe)
+                    if earliest_date.replace(tzinfo=UTC) > fallback_starting_date:
+                        download_from = fallback_starting_date
                 else:
-                    last_date = fallback_starting_date
+                    download_from = fallback_starting_date
 
                 # Download new data and merge with existing data
                 task = (asyncio.create_task(
-                    self.data_downloader.download_one_ticker(ticker, last_date, today_datetime, timeframe)))
+                    self.data_downloader.download_one_ticker(ticker, download_from, today_datetime, timeframe, tqdm_position=len(tasks)-1)))
                 tasks.append(task)
                 properties.append((pair, timeframe))
         tasks = asyncio.as_completed(tasks)
@@ -129,7 +137,7 @@ class BinanceDataProvider:
                 else:
                     self.cached_dataframes[timeframe][pair] = new_data
                 self.cached_dataframes[timeframe][pair].write_csv(ticker_path)
-                print(f"Updated data for {pair}")
+                # print(f"Updated data for {pair}")
             else:
                 print(f"No new data available for {pair}")
 
@@ -146,66 +154,67 @@ class BinanceDataProvider:
 
 class BinanceDataDownloader:
     downloadable_ticker_information = {}
-    pbar = None
+    pbars = None
     use_pbar = True
     __minimum_achieved_date = None
 
-    async def download_and_process(self, session, url: str, ticker: str, date_of_cycle: datetime, is_csv=True):
+    async def download_and_process(self, session, url: str, ticker: str, date_of_cycle: datetime, is_csv=True, tqdm_position=0):
         DEFAULT_COLUMNS = ['open_time', 'open', 'high', 'low', 'close', 'volume',
                            'close_time', 'quote_volume', 'count', 'taker_buy_volume',
                            'taker_buy_quote_volume', 'ignore']
-        try:
-            response = await session.get(url)
-            if response.status_code == 200:
-                if is_csv:
-                    data = response.read()
-                    with zipfile.ZipFile(BytesIO(data)) as zip_file:
-                        with zip_file.open(zip_file.namelist()[0]) as csv_file:
-                            first_line = csv_file.readline().decode("utf-8").strip()
-                            csv_file.seek(0)  # Reset the file pointer to the beginning
+        retry_count = 0
+        while retry_count < self.max_retry_count:
+            try:
+                response = await session.get(url)
+                if response.status_code == 200:
+                    if is_csv:
+                        data = response.read()
+                        with zipfile.ZipFile(BytesIO(data)) as zip_file:
+                            with zip_file.open(zip_file.namelist()[0]) as csv_file:
+                                first_line = csv_file.readline().decode("utf-8").strip()
+                                csv_file.seek(0)  # Reset the file pointer to the beginning
 
-                            if "open_time" in first_line:
-                                df = pl.read_csv(csv_file.read())
-                            else:
-                                df = pl.read_csv(csv_file.read(), has_header=False, new_columns=DEFAULT_COLUMNS)
+                                if "open_time" in first_line:
+                                    df = pl.read_csv(csv_file.read())
+                                else:
+                                    df = pl.read_csv(csv_file.read(), has_header=False, new_columns=DEFAULT_COLUMNS)
+                    else:
+                        data = response.json()
+                        df = pl.DataFrame(data)
+                        df.columns = DEFAULT_COLUMNS
+                    df = df.with_columns((pl.col(coln).cast(pl.Float64) for coln in DEFAULT_COLUMNS if
+                                          not coln in ["open_time", "close_time"]))
+                    df = df.filter(pl.col("ignore") == 0).rename({"open_time": "date"}).drop(
+                        ["close_time", "ignore", "quote_volume", "taker_buy_quote_volume"]).with_columns(
+                        (pl.col("date").cast(pl.Datetime(time_unit="ms")).dt.replace_time_zone("UTC").alias("date")))
+                    # print(df)
+                    if tqdm_position in self.pbars:
+                        self.pbars[tqdm_position].update(1)
+                    if self.__minimum_achieved_date is None or date_of_cycle < self.__minimum_achieved_date:
+                        self.__minimum_achieved_date = date_of_cycle
+                    return df
                 else:
-                    data = response.json()
-                    df = pl.DataFrame(data)
-                    df.columns = DEFAULT_COLUMNS
-                df = df.with_columns((pl.col(coln).cast(pl.Float64) for coln in DEFAULT_COLUMNS if
-                                      not coln in ["open_time", "close_time"]))
-                df = df.filter(pl.col("ignore") == 0).rename({"open_time": "date"}).drop(
-                    ["close_time", "ignore", "quote_volume", "taker_buy_quote_volume"]).with_columns(
-                    (pl.col("date").cast(pl.Datetime(time_unit="ms")).dt.replace_time_zone("UTC").alias("date")))
-                # print(df)
-                if self.pbar is not None:
-                    self.pbar.update(1)
-                if self.__minimum_achieved_date is None or date_of_cycle < self.__minimum_achieved_date:
-                    self.__minimum_achieved_date = date_of_cycle
-                return df
-            else:
-                raise ConnectionError(response.status_code)
-        except Exception as e:
+                    raise ConnectionError(response.status_code)
+            except Exception as e:
+                retry_count += 1
+                if self.pbars[tqdm_position] is not None:
+                    if self.__minimum_achieved_date is not None and (
+                            not date.today() == date_of_cycle.date()) and self.__minimum_achieved_date < date_of_cycle:
+                        self.pbars[tqdm_position].write(f"A hole in the cycle has been found, minimum achieved date is {self.__minimum_achieved_date}, url: {url.split('/klines')[1]}, {e}")
 
-            if self.__minimum_achieved_date is not None and (
-            not date.today() == date_of_cycle.date()) and self.__minimum_achieved_date < date_of_cycle:
-                print(date.today(), date_of_cycle)
-                raise Exception(
-                    f"A hole in the cycle has been found, minimum achieved date is {self.__minimum_achieved_date}, url: {url.split('/klines')[1]}, {e}")
-            if self.pbar is not None:
-                self.pbar.write(f"Error: {url.split('/klines')[1]}, {e}")
-                self.pbar.update(1)
-                if not hasattr(self.pbar, "error_count"):
-                    self.pbar.error_count = 1
+                    self.pbars[tqdm_position].write(f"Error({retry_count}/{self.max_retry_count} retries): {url.split('/klines')[1]}, {e}")
+                    self.pbars[tqdm_position].update(1)
+                    if not hasattr(self.pbars[tqdm_position], "error_count"):
+                        self.pbars[tqdm_position].error_count = 1
+                    else:
+                        self.pbars[tqdm_position].error_count += 1
+                    self.pbars[tqdm_position].set_postfix_str(f"Error Count: {self.pbars[tqdm_position].error_count}")
                 else:
-                    self.pbar.error_count += 1
-                self.pbar.set_postfix_str(f"ErrCount: {self.pbar.error_count}")
-                # self.pbar.
-            else:
-                print(f"Error: Failed to download data for {ticker} from {url}")
-            return pl.DataFrame()
+                    print(f"Error: Failed to download data for {ticker} from {url}")
+        return pl.DataFrame()
 
-    async def download_one_ticker(self, ticker, start_date: datetime, end_date: datetime, timeframe, spot=False):
+    async def download_one_ticker(self, ticker, start_date: datetime, end_date: datetime, timeframe, spot=False, max_retry_count=5, tqdm_position=0):
+        self.max_retry_count = max_retry_count
         # Write some code to ensure that start_date is smaller than end_date and make them both timezone-aware using UTC
         start_date = start_date.replace(tzinfo=UTC)
         end_date = end_date.replace(tzinfo=UTC)
@@ -231,25 +240,25 @@ class BinanceDataDownloader:
                         url = f"https://data-api.binance.vision/api/v3/klines?symbol={ticker}&interval={timeframe}&limit=1000&startTime={start_time}&endTime={end_time}"
                     else:
                         url = f"https://fapi.binance.com/fapi/v1/klines?symbol={ticker}&interval={timeframe}&limit=1000&startTime={start_time}&endTime={end_time}"
-                    task = asyncio.create_task(self.download_and_process(session, url, ticker, current_date, False))
+                    task = asyncio.create_task(self.download_and_process(session, url, ticker, current_date, False, tqdm_position))
                     tasks.append(task)
                     current_date += timedelta(minutes=1000)
                 elif current_date.month == end_date.month and current_date.year == end_date.year:
                     # Download daily data for the ending month
                     url = f"https://data.binance.vision/data/{prefix}/daily/klines/{ticker}/{timeframe}/{ticker}-{timeframe}-{year}-{month:02d}-{day:02d}.zip"
-                    task = asyncio.create_task(self.download_and_process(session, url, ticker, current_date, True))
+                    task = asyncio.create_task(self.download_and_process(session, url, ticker, current_date, True, tqdm_position))
                     tasks.append(task)
                     current_date += timedelta(days=1)
 
                 else:
                     # Download monthly data for full months
                     url = f"https://data.binance.vision/data/{prefix}/monthly/klines/{ticker}/{timeframe}/{ticker}-{timeframe}-{year}-{month:02d}.zip"
-                    task = asyncio.create_task(self.download_and_process(session, url, ticker, current_date, True))
+                    task = asyncio.create_task(self.download_and_process(session, url, ticker, current_date, True, tqdm_position))
                     tasks.append(task)
                     # add one month to the current date setting the day to be the first day of the month
                     current_date = current_date.replace(day=1) + relativedelta(months=1)
             if self.use_pbar:
-                self.pbar = tqdm.tqdm(total=len(tasks),
+                self.pbars[tqdm_position] = tqdm.tqdm(total=len(tasks), position=tqdm_position,
                                       desc=f'DOWN {ticker}-{timeframe}, {start_date.date()} to {end_date.date()}')
 
             dfs = await asyncio.gather(*tasks)
@@ -257,7 +266,8 @@ class BinanceDataDownloader:
             if combined_df.shape[0] == 0:
                 raise Exception(f"No data found for {ticker} between {start_date} and {end_date}")
             if self.use_pbar:
-                self.pbar.close()
+                self.pbars[tqdm_position].close()
+                del self.pbars[tqdm_position]
             return combined_df
 
     async def __fetch_downloadable_tickers(self):
@@ -290,5 +300,7 @@ class BinanceDataDownloader:
             return result["data"]
 
     def __init__(self, use_pbar=True):
+        self.max_retry_count = None
         self.use_pbar = use_pbar
+        self.pbars = {}
         self.downloadable_ticker_information = (asyncio.run(self.__fetch_downloadable_tickers()))
--- tests.py
+++ tests.py
@@ -4,44 +4,48 @@ import asyncio
 
 import polars as pl
 from datetime import datetime
+from shutil import rmtree
 
 
 class TestBinanceDataDownloader(unittest.TestCase):
     def test_downloader_one_ticker(self):
-        from src.binance_data import BinanceDataDownloader
+        from historical_binance import BinanceDataDownloader
         downloader = BinanceDataDownloader()
         data = asyncio.run(downloader.download_one_ticker("BTCUSDT", datetime(2020, 1, 1), datetime(2024, 3, 10), "1d"))
         self.assertTrue(type(data) == pl.DataFrame and data.shape[0] > 0)
 
     def test_downloader_one_ticker_invalid(self):
-        from src.binance_data import BinanceDataDownloader
+        from historical_binance import BinanceDataDownloader
         downloader = BinanceDataDownloader()
         with self.assertRaises(Exception):
             data = asyncio.run(
                 downloader.download_one_ticker("INVALID", datetime(2020, 1, 1), datetime(2020, 1, 31), "1w"))
 
     def test_binance_data_provider(self):
-        from src.binance_data import BinanceDataProvider
+        from historical_binance import BinanceDataProvider
         provider = BinanceDataProvider(["ETHUSDT", "BTC/USDT:USDT", "RUNE/USDT:USDT"], ["1m"])
         asyncio.run(provider.update_tickers(["ETHUSDT", "BTC/USDT:USDT"], ["1m"]))
         self.assertTrue(os.path.exists("./tickers/ETHUSDT-1m.csv"))
+        rmtree("./tickers")
 
     def test_binance_data_provider_async(self):
-        from src.binance_data import BinanceDataProvider
+        from historical_binance import BinanceDataProvider
         provider = BinanceDataProvider(["ETHUSDT", "BTC/USDT:USDT"], ["1m"])
         asyncio.run(provider.update_tickers_async(["ETHUSDT", "BTC/USDT:USDT"], ["1m"], datetime(2023, 2, 1)))
         # print(provider.cached_dataframes["1m"]["ETHUSDT"])
         self.assertTrue(os.path.exists("./tickers/ETHUSDT-1m.csv"))
+        rmtree("./tickers")
 
     def test_binance_data_provider_async_big(self):
-        from src.binance_data import BinanceDataProvider
+        from historical_binance import BinanceDataProvider
         provider = BinanceDataProvider(["ETH/USDT:USDT", "BTC/USDT:USDT", "FET/USDT:USDT", "RUNE/USDT:USDT", "SOL/USDT:USDT", "OP/USDT:USDT"], ["1m"])
         asyncio.run(provider.update_tickers_async(["ETH/USDT:USDT", "BTC/USDT:USDT", "FET/USDT:USDT", "RUNE/USDT:USDT", "SOL/USDT:USDT", "OP/USDT:USDT"], ["1m"], datetime(2023, 2, 1)))
         # print(provider.cached_dataframes["1m"]["ETHUSDT"])
         self.assertTrue(os.path.exists("./tickers/ETHUSDT-1m.csv"))
+        rmtree("./tickers")
 
     def test_binance_data_provider_naming_convention(self):
-        from src.binance_data import BinanceDataProvider
+        from historical_binance import BinanceDataProvider
         TEST_MIN = datetime(2023, 12, 7)
         provider = BinanceDataProvider(["ETHUSDT", "BTC/USDT:USDT", "RUNE/USDT:USDT"], ["1m"], "./data/futures",
                                        "{currency}_USDT_USDT-{timeframe}.csv")
@@ -49,7 +53,13 @@ class TestBinanceDataDownloader(unittest.TestCase):
         # print(provider.cached_dataframes["1m"]["ETHUSDT"])
         self.assertTrue(os.path.exists("./data/futures/ETH_USDT_USDT-1m.csv"))
         self.assertTrue(os.path.exists("./data/futures/BTC_USDT_USDT-1m.csv"))
+        rmtree("./data")
 
 
 if __name__ == '__main__':
+    # from historical_binance import BinanceDataProvider
+    #
+    # provider = BinanceDataProvider(["ETH/USDT:USDT", "BTC/USDT:USDT"], ["1m"], "./data/futures",
+    #                                "{currency}_USDT_USDT-{timeframe}.csv")
+    # asyncio.run(provider.update_tickers_async(["ETH/USDT:USDT", "BTC/USDT:USDT"], ["1m"], datetime(2022, 12, 7)))
     unittest.main()
